30e1891e0215ea080e81018c276d9fdef7f50c96
dmkopylets <dm.kopylets@gmail.com>
2022-12-20 08:45:08 +0200

    added DbConnect.php 2

--- api/index.php
+++ api/index.php
@@ -1,14 +1,12 @@
 <?php
 
-use DbConnect;
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
 
-//include 'DbConnect.php';
+include 'DbConnect.php';
 $objDb = new DbConnect;
 $conn = $objDb->connect();
 $method = $_SERVER['REQUEST_METHOD'];
